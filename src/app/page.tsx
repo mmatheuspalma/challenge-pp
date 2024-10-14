94e478d5b10019dce112a3a01351ddb590f58530
Matheus Palma <mmatheuspalma@gmail.com>
2024-10-14 18:01:24 -0300

    feat: moving business logic to ouside the component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,82 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+
 import useTransactions from "@/services/transactions/useTransactions";
-import { ITransaction } from "./api/transactions/route";
+
+import Modal from "@/components/Modal";
+import Transaction from "@/components/Transaction";
+import Datepicker from "@/components/DatePicker";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [transactions, setTransactions] = useState<ITransaction[]>([]);
-  const transactionsService = useTransactions();
+  const {
+    loading,
+    setLoading,
+    transactions,
+    transactionError,
+    transactionErrorOpened,
+    setTransactionErrorOpened,
+    getAllTransactions,
+    transactionDateStart,
+    setTransactionDateStart,
+    transactionDateEnd,
+    setTransactionDateEnd,
+  } = useTransactions();
 
-  const getAllTransactions = async () => {
-    setLoading(true);
-
-    try {
-      const items = await transactionsService.getTransactions({});
-
-      setTransactions(items);
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false);
-    }
-  }
+  const [forcedApiError, setForcedApiError] = useState<boolean>(false);
 
   useEffect(() => {
-    getAllTransactions();
-  }, [])
+    getAllTransactions(forcedApiError);
+  }, [forcedApiError])
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="w-[100%]">
+        <div className="w-[100%] mb-4 flex flex-row space-between">
+          <button
+            className={`${forcedApiError ? 'bg-[red]' : 'bg-[blue]'} text-white py-2 px-4 rounded`}
+            onClick={() => setForcedApiError(!forcedApiError)}
+          >
+            Force API Error
+          </button>
+          {transactionDateStart?.toDateString()}
+          {transactionDateEnd?.toDateString()}
+          <Datepicker
+            startDate={transactionDateStart}
+            endDate={transactionDateEnd}
+            onChangeStartDate={setTransactionDateStart}
+            onChangeEndDate={setTransactionDateEnd}
+            multiple
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          {
+            transactions.length ?
+              transactions.map((t, index) => (
+                <Transaction
+                  key={index}
+                  id={t.id}
+                  description={t.description}
+                  amount={t.amount}
+                  date={t.date}
+                />
+              ))
+            : (
+              <div>
+                Oops, empty list!
+              </div>
+            )
+          }
+        </div>
         {
-          transactions.map((t, index) => (
-            <div className="w-[200px] h-[100px] bg-white rounded" key={index}>
-              {t.id} - {t.amount}
-            </div>
-          ))
+          transactionError && (
+            <Modal
+              close={() => setTransactionErrorOpened(false)}
+              isOpen={transactionErrorOpened}
+              title={transactionError.message}
+              description={transactionError.cause}
+            />
+          )
         }
       </main>
     </div>
--- src/services/transactions/useTransactions.ts
+++ src/services/transactions/useTransactions.ts
@@ -1,17 +1,91 @@
+import { useMemo, useState } from "react";
+
 import { ITransaction } from "@/app/api/transactions/route";
 
+type TransactionError = {
+  cause: string;
+  message: string;
+};
+
+type TransactionFilter = {
+  field: string;
+  value: string;
+};
+
 const useTransactions = () => {
-  const getTransaction = async (transactionId: ITransaction['id']) => {
-    return (await fetch(`http://localhost:3000/api/transactions/${transactionId}`)).json();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  const [transactionError, setTransactionError] = useState<TransactionError>();
+  const [transactionErrorOpened, setTransactionErrorOpened] = useState<boolean>(false);
+  const [transactionFilters, setTransactionFilters] = useState<TransactionFilter>();
+  const [transactionDateStart, setTransactionDateStart] = useState<Date>(new Date());
+  const [transactionDateEnd, setTransactionDateEnd] = useState<Date>();
+
+  // memo manipulating
+  const transactionsFiltered = useMemo(() => {
+    if (!transactionDateStart || !transactionDateEnd) {
+      return transactions;
+    }
+
+    return transactions.filter((t: ITransaction) => {
+      return t.date >= new Date(transactionDateStart) && t.date <= new Date(transactionDateEnd);
+    })
+  }, [transactionDateStart, transactionDateEnd, transactions]);
+
+  const getTransactionsList = async (): Promise<ITransaction[]> => {
+    return await (await fetch('/api/transactions')).json();
   }
 
-  const getTransactions = async (filters: any) => {
-    return (await fetch('http://localhost:3000/api/transactions', filters)).json();
-  }
+  const getAllTransactions = async (forcedApiError: boolean = false): Promise<ITransaction[] | void> => {
+    setLoading(true);
+
+    try {
+      if (forcedApiError) {
+        setTransactions([]);
+        throw new Error('Forced Error', {
+          cause: 'the getAllTransactions method was forced to return an error'
+        });
+      }
+
+      const items = await getTransactionsList();
+
+      setTransactions(items);
+
+      return items;
+    } catch (error) {
+      if (error instanceof Error) {
+        setTransactionError({
+          cause: error.cause as string,
+          message: error.message,
+        });
+      } else {
+        setTransactionError({
+          cause: 'getAllTransactions had an generic error',
+          message: 'Error'
+        })
+      }
+
+      setTransactionErrorOpened(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return {
-    getTransaction,
-    getTransactions,
+    loading,
+    setLoading,
+    transactionError,
+    setTransactionError,
+    transactionErrorOpened,
+    setTransactionErrorOpened,
+    transactionFilters,
+    setTransactionFilters,
+    transactionDateStart,
+    setTransactionDateStart,
+    transactionDateEnd,
+    setTransactionDateEnd,
+    getAllTransactions,
+    transactions: transactionsFiltered,
   }
 };
 
